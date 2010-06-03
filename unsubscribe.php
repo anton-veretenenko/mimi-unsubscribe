3cb61745a3a7627eecaf3fc6d9b038305d7b1b1b
Anton Veretenenko <anton@veretenenko.ru>
2010-06-04 02:51:53 +0600

    Modified for onepage mode, with 'email' GET parameter. If array of lists defined then lists sorts like you define them. Redirects changed to errors.

--- unsubscribe.php
+++ unsubscribe.php
@@ -3,6 +3,12 @@
 	require_once 'config.php';
 
 	$args = $_POST;
+	if (!empty($_GET['email'])) {
+		$args['email'] = $_GET['email'];
+		if (!empty($_SESSION['email'])) {
+			unset($_SESSION['email']);
+		}
+	}
 
 	if (!empty($args) && !empty($args['email']) && $mimi->Authenticate() && $mimi->HaveUser($args['email'])) {
 		if (!empty($_SESSION['email'])) {
@@ -28,7 +34,7 @@
 				foreach ($lists as $id => $name) {
 					if ($edit_lists[$id] === '0' && !empty($user_lists[$id])) {
 						// process unsubscribe
-						$mimi->RemoveUser($email, $name);
+						$mimi->RemoveUser($email, $name, true);
 					} else
 					if ($edit_lists[$id] === '1' && empty($user_lists[$id])) {
 						// process subscribe
@@ -39,20 +45,22 @@
 						$user['add_list'] = $name;
 						$mimi->AddUser($user);
 					}/* else {
-						// if parameneters are bad then redirect to index
-						header('Location: index.php');
+						// if parameneters are bad then show error
+						$error = 'not-found';
 					}*/
 				}
 				$user_lists = prepareLists(new SimpleXMLElement($mimi->Memberships($email)));
 			} else {
-				// if emails does not match then redirect to index
-				//header('Location: index.php');
+				// if emails does not match then show error
+				$error = 'not-found';
 			}
 		}
 	} else {
 		// if no data posted or mimi authentication failed or
-		// mimi have not such email then redirect to index
-		header('Location: index.php');
+		// mimi have not such email then show error
+		// just error code defined, error message for error code should
+		// be defined in template
+		$error = 'not-found';
 	}
 
 	function prepareLists(SimpleXMLElement $lists)
@@ -67,10 +75,12 @@
 	function prepareListsByNames(SimpleXMLElement $all_lists, $lists_names)
 	{
 		$new_lists = array();
-		foreach($all_lists as $list) {
-			foreach ($lists_names as $name) {
+		$temp_lists = $all_lists;
+		foreach ($lists_names as $name) {
+			foreach ($temp_lists as $i => $list) {
 				if (strcmp($list['name'], $name) === 0) {
 					$new_lists[(string)$list['id']] = (string)$list['name'];
+					unset($temp_lists[$i]);
 				}
 			}
 		}
